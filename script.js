bb8c60ccd98a4c5528e07e67160c3db60a982194
Marcoshlp18 <marcoshenriqueball12@gmail.com>
2022-06-14 23:28:56 -0300

    Atividade de loops

---
+++ script.js
@@ -0,0 +1,19 @@
+let i= 1;
+for( let i= 1; i<=10; i++) {
+   console.log("1 até 10 com for:",i);
+}
+let x=10;
+while(x>=1){
+  console.log("10 até 1 com for:",x);
+  x--;
+}
+
+let t=1;
+for(t;t<=100; t +=2){
+  console.log("1 até 99 somente ímpares:",t);
+}
+
+let p=0;
+for(p;p<=100; p +=2){
+  console.log("0 até 100 somente pares:",p);
+}
